a26dd0690b9a2cf88d644db2988445b8a1e3abc1
zainoor <rzainoor28@gmail.com>
2025-12-22 15:09:20 +0700

    Add Git Ignore and Revert Mistakes

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,8 +2,6 @@ import os
 import joblib
 import mlflow
 import mlflow.sklearn
-mlflow.set_tracking_uri("file://" + os.path.abspath("mlruns"))
-import numpy as np
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
