92ac63a69af5b495769270d397c72ec61426f622
zainoor <rzainoor28@gmail.com>
2025-12-22 15:16:42 +0700

    Fix Error

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,6 +2,7 @@ import os
 import joblib
 import mlflow
 import mlflow.sklearn
+import numpy as np
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
@@ -13,22 +14,20 @@ X_test  = joblib.load(os.path.join(DATA_DIR, "X_test.pkl"))
 y_train = joblib.load(os.path.join(DATA_DIR, "y_train.pkl"))
 y_test  = joblib.load(os.path.join(DATA_DIR, "y_test.pkl"))
 
-with mlflow.start_run():
+model = LogisticRegression(
+    max_iter=1000,
+    class_weight="balanced",
+    random_state=42
+)
+model.fit(X_train, y_train)
 
-    model = LogisticRegression(
-        max_iter=1000,
-        class_weight="balanced",
-        random_state=42
-    )
-    model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-    y_pred = model.predict(X_test)
+mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
+mlflow.log_metric("precision", precision_score(y_test, y_pred, pos_label="Yes"))
+mlflow.log_metric("recall", recall_score(y_test, y_pred, pos_label="Yes"))
+mlflow.log_metric("f1_score", f1_score(y_test, y_pred, pos_label="Yes"))
 
-    mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
-    mlflow.log_metric("precision", precision_score(y_test, y_pred, pos_label="Yes"))
-    mlflow.log_metric("recall", recall_score(y_test, y_pred, pos_label="Yes"))
-    mlflow.log_metric("f1_score", f1_score(y_test, y_pred, pos_label="Yes"))
+mlflow.sklearn.log_model(model, "model")
 
-    mlflow.sklearn.log_model(model, "model")
-
-    print("CI training selesai.")
+print("CI training selesai.")
